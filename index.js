e9b2e6de6b1b391cc36c69224e7121c8302616ef
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2023-01-17 23:18:18 -0500

    render build 2

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 var cors = require('cors') 
-const keepAlive = require("./keepAlive.js");
 // for methods other than GET!!!
 
 var bodyParser = require('body-parser');
@@ -11,9 +10,4 @@ app.use(cors()); // for methods other than GET!!!
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use('/', game);
-
-app.listen(8000, () => {
-  console.log('Server Awake');
-});
