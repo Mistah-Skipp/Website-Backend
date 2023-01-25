dd1934565301a4690d55a1b4bdd250982a5e79e8
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2023-01-24 22:36:53 -0500

    reworked webserver

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-var cors = require('cors') 
-// for methods other than GET!!!
-
-var bodyParser = require('body-parser');
-
-const app = express();
+const bodyParser = require('body-parser');
+const app = express()
 const port = 5001;
-app.listen(port);
-const game = require('./dbStuff.js');
-app.use(cors()); // for methods other than GET!!!
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.game;
+const db = require("/dbStuff.js");
+
+app.db;
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({extended:true,})
+)
+app.listen(port, ()=>{console.log("server running on port "+port)})
+
+app.get('/', (req,res) =>{
+    res.redirect("/index.html");
+})
