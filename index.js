bdfdc0b3d13369f2fcf29416f43c698db0b709af
Kenny <56985880+Mistah-Skipp@users.noreply.github.com>
2023-01-25 17:44:44 -0500

    made public folder for website

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
     bodyParser.urlencoded({extended:true,})
 )
 app.listen(port, ()=>{console.log("server running on port "+port)})
-
+app.use(express.static(path.join(__dirname,"public")));
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '/index.html'));
     console.log(__dirname);
